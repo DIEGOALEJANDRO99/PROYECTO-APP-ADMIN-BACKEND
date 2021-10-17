8ad8cb3b410a3bd58bc68fd01c2cb3db853e39b5
Diego Alejandro Malagón <diego.dma99@gmail.com>
2021-10-16 22:52:26 -0500

    Setup ok with jwt

--- authProject/settings.py
+++ authProject/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -41,6 +43,17 @@ INSTALLED_APPS = [
     'authApp'
 
 ]
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': True,
+    'UPDATE_LAST_LOGIN': False,
+    'ALGORITHM': 'HS256',
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -52,6 +65,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+REST_FRAMEWORK = {
+
+    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.AllowAny'),
+    'DEFAULT_AUTHENTICATION_CLASSES':('rest_framework_simplejwt.authentication.JWTAuthentication'),
+
+}
 ROOT_URLCONF = 'authProject.urls'
 
 TEMPLATES = [
@@ -78,8 +97,12 @@ WSGI_APPLICATION = 'authProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME':  'dokjuv6qbcno9',
+        'USER': 'ayvzelnqbnbjwc',
+        'PASSWORD': 'bbf9aa6c6360439f198104f90436bd36a2feed872422d11e866e6adb98b9e4f3',
+        'HOST': 'ec2-34-197-135-44.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
